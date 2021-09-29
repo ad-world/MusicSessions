eb6a81d2520c56a40835bda9cae56b79064351db
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-29 14:18:12 -0400

    Setting up tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	purge    : [],
+	darkMode : false, // or 'media' or 'class'
+	theme    : {
+		extend : {
+			outline : {
+				white : '2px solid #FFFFFF'
+			}
+		},
+		colors : {
+			green : {
+				spotify : '#1DB954'
+			},
+			blue  : {
+				light : '#0D1A8E',
+				dark  : '#13043F'
+			},
+			white : '#FFFFFF'
+		}
+	},
+	variants : {
+		extend : {}
+	},
+	plugins  : []
+};
