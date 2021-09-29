73b48cddc30472e3e8af751b8bd6f05a3626eefd
ad-world <67097683+ad-world@users.noreply.github.com>
2021-09-29 14:21:58 -0400

    Added sample routes

--- server/routes/main/main.js
+++ server/routes/main/main.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+	res.render('signin/signin', { layout: 'home/main' });
+});
+
 router.get('/home', (req, res) => {
 	res.render('signin/signin', { layout: 'home/main' });
 });
