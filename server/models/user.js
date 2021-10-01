425213879eb316b7529beb76c8eb9c176875f5f2
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-01 17:27:51 -0400

    Fix user model field issues

--- server/models/user.js
+++ server/models/user.js
@@ -8,7 +8,7 @@ const UserSchema = new Schema({
 	id            : String,
 	email         : { type: String, required: true },
 	spotify_id    : String,
-	access_token  : { type: String, required: true },
+	token  : { type: String, required: true },
 	refresh_token : { type: String, required: true },
 	online        : { type: Boolean, default: false }
 });
