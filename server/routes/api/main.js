86e7599a2adf0fd8c5b63cbc07a95f6cd689e855
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-02 12:34:57 -0400

    Import user api routes

--- server/routes/api/main.js
+++ server/routes/api/main.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const spotify = require('./spotify/main');
+const users = require('./users/main');
 
 router.use('/api', spotify);
+router.use('/api', users);
 
 module.exports = router;
