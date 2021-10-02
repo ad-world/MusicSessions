156068deaa5439961b35162a4c1b27010fe337b8
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-02 14:45:42 -0400

    Change render based on session tokens

--- server/routes/main/home/main.js
+++ server/routes/main/home/main.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-	res.render('home/landing', { layout: 'home/main' });
+	if (req.session.token && req.session.refresh_token) {
+		const name = req.session.name;
+		res.render('home/dashboard', { name: name, layout: 'home/main' });
+	} else {
+		res.render('home/landing', { layout: 'home/main' });
+	}
 });
 
 router.get('/create', (req, res) => {
