93c36ad2e59e043f0c3ca1dc2084f7d6624c5b2a
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-02 12:40:42 -0400

    Add token and refresh token to backend session

--- server/routes/api/users/main.js
+++ server/routes/api/users/main.js
@@ -2,7 +2,13 @@ const router = require('express').Router();
 
 router.get('/login', (req, res) => {
 	let params = req.query;
-	res.send(params);
+
+	let session = req.session;
+	session.token = params.token;
+	session.name = params.name;
+	session.refresh_token = params.refresh_token;
+
+	res.send(req.session);
 });
 
 module.exports = router;
