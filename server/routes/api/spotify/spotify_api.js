98a84ef91d887ee08941bc52ccd9a424e19299a9
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-03 18:32:20 -0400

    Create spotify search route

---
+++ server/routes/api/spotify/spotify_api.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const url = require('url');
+const axios = require('axios');
+const util = require('../util/auth');
+
+router.get('/search', util.authenticated, async (req, res) => {
+	const keywords = req.query.keywords;
+	const token = req.session.token;
+	console.log(keywords);
+	var data;
+	try {
+		const options = {
+			method  : 'GET',
+			headers : {
+				Authorization  : 'Bearer ' + token,
+				'Content-Type' : 'application/json',
+				Accept         : 'application/json'
+			},
+			url     : `${process.env.SPOTIFY_API}/search?q=` + encodeURIComponent(keywords) + '&type=track'
+		};
+
+		await axios(options).then((result) => (data = result.data));
+	} catch (err) {
+		console.error(err);
+	}
+	res.send(data);
+});
+
+module.exports = router;
