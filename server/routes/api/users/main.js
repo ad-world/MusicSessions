aef67acd7864b725b69434db8b535a049f53f3e5
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-12 19:21:09 -0400

    Check if product is premium

--- server/routes/api/users/main.js
+++ server/routes/api/users/main.js
@@ -3,18 +3,21 @@ const router = require('express').Router();
 router.get('/login', (req, res) => {
 	let params = req.query;
 
+	if (params.product !== 'premium') {
+		return res.redirect('/?error=not_premium');
+	} 
 	let session = req.session;
 	session.user_id = params.id;
 	session.token = params.token;
 	session.name = params.name;
 	session.refresh_token = params.refresh_token;
 
-	res.redirect('/');
+	return res.redirect('/');
 });
 
 router.get('/logout', (req, res) => {
 	req.session.destroy();
-	res.redirect('/');
+	return res.redirect('/');
 });
 
 module.exports = router;
