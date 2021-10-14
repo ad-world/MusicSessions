7554e938f153b7bbb3b866e9f3c30e9a34322e13
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-14 17:08:46 -0400

    Check host before rendering session page

--- server/routes/main/session/main.js
+++ server/routes/main/session/main.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 const util = require('../../util/auth');
+const queue_logic = require('../../../controllers/queue/logic');
 
-router.get('/session/:session_id', util.view_auth, (req, res) => {
+router.get('/session/:session_id', util.view_auth, async (req, res) => {
 	const session_id = req.params.session_id;
 	const user_id = req.session.user_id;
 
+	const validation = await queue_logic.check_host(user_id, session_id);
 
-	res.render('home/session', { layout: 'home/session' });
+	if (validation.status == 'success') {
+		res.render('home/session', { layout: 'home/session' });
+	} else {
+		res.render('error/error', { layout: 'home/main' });
+	}
 });
 
 module.exports = router;
