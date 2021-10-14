5077f9b8a0b2deb1405258189c1648960a188eac
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-13 23:48:30 -0400

    Changed util location

---
+++ server/routes/util/auth.js
@@ -0,0 +1,22 @@
+function authenticated (req, res, next) {
+	if (req.session.token && req.session.refresh_token && req.session.user_id) {
+		next();
+	} else {
+		req.session.destroy();
+		res.status(401).send({
+			status  : 'failure',
+			message : 'Unauthenticated request'
+		});
+	}
+}
+
+function view_auth (req, res, next) {
+	if (req.session.token && req.session.refresh_token && req.session.user_id) {
+		next();
+	} else {
+		req.session.destroy();
+		res.render('home/landing', { layout: 'home/main' });
+	}
+}
+
+module.exports = { authenticated, view_auth };
