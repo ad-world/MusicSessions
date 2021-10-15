3a65207a9f874477b2ea44d2b81a289b1a99353b
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-15 13:22:30 -0400

    Create join session function

--- server/controllers/queue/logic.js
+++ server/controllers/queue/logic.js
@@ -1,4 +1,5 @@
 const Queue = require('../../models/queue');
+const uniqid = require('uniqid');
 
 async function check_host (host_id, queue_id) {
 	let queue = await Queue.findOne({ id: queue_id }).lean().exec();
@@ -24,4 +25,39 @@ async function check_host (host_id, queue_id) {
 	};
 }
 
-module.exports = { check_host };
+async function join_session (data) {
+	const { name, join_id } = data;
+
+	let queue = await Queue.findOne({ join_id: join_id }).lean().exec();
+	if (!queue) {
+		return {
+			status  : 'failure',
+			message : 'The session code is invalid.'
+		};
+	}
+
+	const user = {
+		id   : uniqid(),
+		name : name
+	};
+
+	const updates = {
+		$push : {
+			ragers : user
+		},
+		$inc  : {
+			size : 1
+		}
+	};
+
+	await Queue.updateOne({ join_id: join_id }, updates).lean().exec();
+
+	return {
+		status   : 'success',
+		message  : 'Session has been joined.',
+		id       : user.id,
+		queue_id : queue.id
+	};
+}
+
+module.exports = { check_host, join_session };
