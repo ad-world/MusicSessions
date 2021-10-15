9e3a323bc9f83661e385561c5e744a6aa9e9ddf4
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-15 13:22:49 -0400

    Create join session route

--- server/routes/api/sessions/main.js
+++ server/routes/api/sessions/main.js
@@ -1,13 +1,30 @@
 const router = require('express').Router();
 const util = require('../../util/auth');
 const queue_actions = require('../../../controllers/queue/actions');
+const queue_logic = require('../../../controllers/queue/logic');
 
 router.post('/session/create', util.authenticated, async (req, res) => {
 	const id = req.session.user_id;
 	const queue = await queue_actions.create_queue(id);
-    
+
 	return res.send(queue);
 });
 
+router.post('/session/join', async (req, res) => {
+	const { name, join_id } = req.body;
 
+	const data = {
+		name    : name,
+		join_id : join_id
+	};
+
+	const response = await queue_logic.join_session(data);
+
+	if (response.id) {
+		req.session.name = name;
+		req.session.id = response.id;
+	}
+
+	return res.send(response);
+});
 module.exports = router;
