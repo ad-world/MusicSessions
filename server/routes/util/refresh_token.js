02382d3a518fd7780b2f091fac9d87a187ce67ae
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-17 14:17:48 -0400

    Fix refresh token middleware

--- server/routes/util/refresh_token.js
+++ server/routes/util/refresh_token.js
@@ -1,51 +1,13 @@
 const request = require('request');
 const querystring = require('querystring');
-
-// async function refresh (req, res, next) {
-// 	console.log(token);
-
-// 	const headers = {
-// 		Authorization  : 'Basic ' + token,
-// 		'Content-Type' : 'application/x-www-form-urlencoded'
-// 	};
-
-// 	const refreshBody = {
-// 		grant_type    : 'refresh_token',
-// 		refresh_token : refresh_token
-// 	};
-
-// 	axios.post('https://accounts.spotify.com/api/token', refreshBody, headers).then((res) => console.log(res));
-
-// 	// console.log(token);
-// 	// var current = Date.now();
-// 	// const options = {
-// 	// 	hostname: 'accounts.spotify.com',
-// 	// 	path: '/api/token',
-// 	// 	method: 'POST'
-// 	// }
-
-// 	// var new_token = await axios.post('https://accounts.spotify.com/api/token', refreshBody, {
-// 	// 	headers : {
-// 	// 		Authorization : 'Basic ' + token
-// 	// 	}
-// 	// });
-
-// 	// console.log(new_token);
-
-// 	next();
-
-// 	// let new = await axios
-// 	// if (current - started_at < expires_in) {
-// 	// 	if (expires_in - (current - started_at) < 300000) {
-// 	// 		// 5 mins
-
-// 	// 	}
-// 	// }
-// }
+const user_actions = require('../../controllers/users/actions');
 
 function refresh (req, res, next) {
-	var { expires_in, started_at, refresh_token } = req.session;
+	var { expires_in, started_at, refresh_token, user_id } = req.session;
 
+	let current = Date.now();
+
+	// if (current - started_at > expires_in || expires_in - (current - started_at) < 300000) {
 	const token = Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64');
 
 	const refreshBody = querystring.stringify({
@@ -54,33 +16,36 @@ function refresh (req, res, next) {
 	});
 	const boom = request(
 		{
-			// Assuming you have this setup as: https://accounts.spotify.com/api/token
 			url     : 'https://accounts.spotify.com/api/token',
 			method  : 'POST',
 			headers : {
-				// Authorization: Basic <base64 encoded client_id:client_secret>
 				Authorization    : 'Basic ' + token,
 				'Content-Type'   : 'application/x-www-form-urlencoded',
 				'Content-Length' : Buffer.byteLength(refreshBody)
 			}
 		},
-		(err, res) => {
-			if (res) {
-				const resData = JSON.parse(res.body);
-				console.log(resData);
-				// Set new access tokens
-				// access_token = resData.access_token;
-				// // setup your Authorization token, e.g.
-				// token = btoa(access_token);
-				// callback();
+		async (err, data) => {
+			if (data) {
+				const resData = JSON.parse(data.body);
+				const updates = {
+					token : resData.access_token
+				};
+
+				await user_actions.update_user(user_id, updates);
+
+				req.session.access_token = resData.access_token;
+				req.session.expires_in = resData.expires_in;
+
 				next();
 			} else if (err) {
-				// Handle error...
-				console.log(err);
+				res.redirect('/api/logout');
 			}
 		}
 	);
 	boom.write(refreshBody);
+	// } else {
+	// 	next();
+	// }
 }
 
 module.exports = { refresh };
