6f21474fd7e0ce4e33b1ccc1841d4b8de2864e47
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-17 17:32:42 -0400

    Remove logs from message

--- public/js/util/message.js
+++ public/js/util/message.js
@@ -1,21 +1,10 @@
 function message (errorDiv, header, headerVal, error, errorVals) {
-	var headerRef = $(`#${header}`);
-	var errorRef = $(`#${error}`);
-
-	console.log(headerRef);
-	console.log(errorRef);
-
-	console.log(headerVal);
-	console.log(errorVals);
-
 	$(`#${header}`).html(headerVal);
 
-	var errorRef = $(`#${error}`);
 	var errorString = '';
 	for (var i = 0; i < errorVals.length; i++) {
 		errorString += errorVals[i];
 	}
-	console.log(errorString);
 	$(`#${error}`).html(errorString);
 
 	$(`#${errorDiv}`).removeClass('hidden');
