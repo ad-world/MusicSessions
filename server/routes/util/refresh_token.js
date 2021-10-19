6f840d509e080b2ab5fad4ebbe94b44ccc582046
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-19 02:38:17 -0400

    Fix refresh token function

--- server/routes/util/refresh_token.js
+++ server/routes/util/refresh_token.js
@@ -7,45 +7,52 @@ function refresh (req, res, next) {
 
 	let current = Date.now();
 
-	// if (current - started_at > expires_in || expires_in - (current - started_at) < 300000) {
-	const token = Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64');
+	if (current - started_at > expires_in || expires_in - (current - started_at) < 300000) {
+		console.log('Current time: ', current);
+		console.log('Started at: ', started_at);
+		console.log('Expires in: ', expires_in);
 
-	const refreshBody = querystring.stringify({
-		grant_type    : 'refresh_token',
-		refresh_token : refresh_token
-	});
-	const boom = request(
-		{
-			url     : 'https://accounts.spotify.com/api/token',
-			method  : 'POST',
-			headers : {
-				Authorization    : 'Basic ' + token,
-				'Content-Type'   : 'application/x-www-form-urlencoded',
-				'Content-Length' : Buffer.byteLength(refreshBody)
+		const token = Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64');
+
+		const refreshBody = querystring.stringify({
+			grant_type    : 'refresh_token',
+			refresh_token : refresh_token
+		});
+		const boom = request(
+			{
+				url     : 'https://accounts.spotify.com/api/token',
+				method  : 'POST',
+				headers : {
+					Authorization    : 'Basic ' + token,
+					'Content-Type'   : 'application/x-www-form-urlencoded',
+					'Content-Length' : Buffer.byteLength(refreshBody)
+				}
+			},
+			async (err, data) => {
+				if (data) {
+					const resData = JSON.parse(data.body);
+					const updates = {
+						token : resData.access_token
+					};
+
+					await user_actions.update_user(user_id, updates);
+
+					req.session.token = resData.access_token;
+					req.session.expires_in = (resData.expires_in * 1000);
+					req.session.started_at = Date.now();
+
+					next();
+				} else if (err) {
+					console.log(err);
+					res.send(err);
+					next();
+				}
 			}
-		},
-		async (err, data) => {
-			if (data) {
-				const resData = JSON.parse(data.body);
-				const updates = {
-					token : resData.access_token
-				};
-
-				await user_actions.update_user(user_id, updates);
-
-				req.session.access_token = resData.access_token;
-				req.session.expires_in = resData.expires_in;
-
-				next();
-			} else if (err) {
-				res.redirect('/api/logout');
-			}
-		}
-	);
-	boom.write(refreshBody);
-	// } else {
-	// 	next();
-	// }
+		);
+		boom.write(refreshBody);
+	} else {
+		next();
+	}
 }
 
 module.exports = { refresh };
