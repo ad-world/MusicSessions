a58cb10068acd26a76f59b76c5e65ff56a38a606
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-18 21:57:39 -0400

    Refactor search route and add user search route

--- server/routes/api/spotify/spotify_api.js
+++ server/routes/api/spotify/spotify_api.js
@@ -1,36 +1,77 @@
 const router = require('express').Router();
 const url = require('url');
 const spotify_actions = require('../../../controllers/spotify/actions');
+const queue_logic = require('../../../controllers/queue/logic');
 const util = require('../../util/auth');
 
 router.get('/search', util.authenticated, async (req, res) => {
-	const keywords = req.query.keywords;
-	const token = req.session.token;
+	try {
+		const keywords = req.query.keywords;
+		const token = req.session.token;
 
-	var data = await spotify_actions.host_search(keywords, token);
+		var data = await spotify_actions.host_search(keywords, token);
 
-	if (data) {
-		data = data.tracks.items;
-		data = data.map((item) => {
-			return {
-				id      : item.id,
-				uri     : item.url,
-				name    : item.name,
-				artists : item.artists.map((item) => item.name),
-				image   : item.album.images[2].url
+		if (data) {
+			data = data.tracks.items;
+			data = data.map((item) => {
+				return {
+					id      : item.id,
+					uri     : item.url,
+					name    : item.name,
+					artists : item.artists.map((item) => item.name),
+					image   : item.album.images[2].url
+				};
+			});
+
+			const response = {
+				items : data
 			};
-		});
 
-		const response = {
-			items : data
-		};
+			res.send(response);
+		} else {
+			res.send({
+				status  : 'failure',
+				message : 'No songs found.'
+			});
+		}
+	} catch (err) {
+		console.error(err);
+	}
+});
 
-		res.send(response);
-	} else {
-		res.send({
-			status  : 'failure',
-			message : 'No songs found.'
-		});
+router.get('/user/search', util.search_auth, async (req, res) => {
+	try {
+		const queue_id = req.session.queue_id;
+		const keywords = req.query.keywords;
+		const token = await queue_logic.get_token_from_queue(queue_id);
+
+		var data = await spotify_actions.host_search(keywords, token);
+
+		if (data) {
+			data = data.tracks.items;
+			data = data.map((item) => {
+				return {
+					id      : item.id,
+					uri     : item.url,
+					name    : item.name,
+					artists : item.artists.map((item) => item.name),
+					image   : item.album.images[2].url
+				};
+			});
+
+			const response = {
+				items : data
+			};
+
+			res.send(response);
+		} else {
+			res.send({
+				status  : 'failure',
+				message : 'No songs found.'
+			});
+		}
+	} catch (err) {
+		console.error(err);
 	}
 });
 
