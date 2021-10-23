c8557a71f139b7cb49afcf9a4ffab58f1516e618
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-23 15:35:13 -0400

    Fix currently playing

--- server/controllers/spotify/actions.js
+++ server/controllers/spotify/actions.js
@@ -22,6 +22,7 @@ async function search (keywords, token) {
 }
 
 async function currently_playing (token) {
+	console.log(token);
 	var data;
 	try {
 		const options = {
@@ -31,14 +32,15 @@ async function currently_playing (token) {
 				'Content-Type' : 'application/json',
 				Accept         : 'application/json'
 			},
-			url     : `${process.env.SPOTIFY_API}/me/player/currently-playing`
+			url     : `${process.env.SPOTIFY_API}/me/player`
 		};
 
-		await axios(options).then((result) => (data = result.data ? result.data : false));
+		await axios(options).then((res) => (data = res.data));
 	} catch (err) {
-		console.error(err.response);
+		console.error(err);
 	}
 
+	console.log(data);
 	return data;
 }
 module.exports = { search, currently_playing };
