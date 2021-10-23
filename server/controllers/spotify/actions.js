ce19bd5f4a177eeb33dd8fab67de105f3b9aed27
ad-world <67097683+ad-world@users.noreply.github.com>
2021-10-23 14:44:00 -0400

    Create currently playing function

--- server/controllers/spotify/actions.js
+++ server/controllers/spotify/actions.js
@@ -18,7 +18,27 @@ async function host_search (keywords, token) {
 		console.error(err.response);
 	}
 
-    return data;
+	return data;
 }
 
-module.exports = {host_search}
+async function currently_playing (token) {
+	var data;
+	try {
+		const options = {
+			method  : 'GET',
+			headers : {
+				Authorization  : 'Bearer ' + token,
+				'Content-Type' : 'application/json',
+				Accept         : 'application/json'
+			},
+			url     : `${process.env.SPOTIFY_API}/me/player/currently-playing`
+		};
+
+		await axios(options).then((result) => (data = result.data ? result.data : false));
+	} catch (err) {
+		console.error(err.response);
+	}
+
+	return data;
+}
+module.exports = { host_search, currently_playing };
