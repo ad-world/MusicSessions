b52b85995b5dac2f8ec863d1914b260242d7387f
Aryaman Dhingra <aryamandhingra@gmail.com>
2021-12-29 13:00:25 +0400

    Fix callback url

--- server/routes/api/spotify/auth.js
+++ server/routes/api/spotify/auth.js
@@ -17,7 +17,7 @@ var persistable = {};
 var callbackUrl = 'http://localhost:3000';
 
 if (process.env.NODE_ENV == 'production') {
-	callbackUrl = 'https://crafty-haiku-331014.nn.r.appspot.com';
+	callbackUrl = 'https://musicsessions.tech';
 }
 
 passport.use(
